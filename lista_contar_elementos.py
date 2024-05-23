03922f7ea61c2d491f68f668f2f2fb4e35397b0a
glendagois29 <glenda.gois07@gmail.com>
2024-05-23 10:49:18 -0300

    criação contar elementos

---
+++ lista_contar_elementos.py
@@ -0,0 +1,4 @@
+alunos= ['Adriana', 'joão', 'maria',' pedro', 'ana', ' lucas', 'mariana', 'adriana']
+
+ocorrencias = alunos.count('adriana')
+print(f' Quantidade de ocorrências de Adriana: {ocorrencias}')
