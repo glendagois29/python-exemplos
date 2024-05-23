cb525cc391cdf8397781c85c8e264d5ebcd901fe
glendagois29 <glenda.gois07@gmail.com>
2024-05-23 09:48:06 -0300

    listas remover

---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos=['Adriana', 'joão', 'maria',' pedro', 'ana', ' lucas', 'mariana', 'adriana']
+print(f' Lista de alunos:  {alunos}')
+#metodo remove()
+alunos.remove(' Adriana')
+print(f' Lista de alunos: {alunos}')
+#metodo pop
+alunos.pop(2)
+print(f' lista de alunos: {alunos}')
+#função del
+del alunos[3]
+print(f' lista de alunos: { alunos}')
+#medoto clear()
+alunos.clear()
+print(f" lista de alunos: {alunos}")
